faf2dd40c283ab851f355f925de5087c6cc20597
dogfun <dog@w2w.fun>
2021-09-17 22:35:50 +0800

    init clweb

---
+++ src/views/kline/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <stock-list />
+  </div>
+</template>
+
+<script>
+import stockList from './components/stockList'
+
+export default {
+  name: 'KLine',
+  components: { stockList },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.documentation-container {
+  margin: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+
+  .document-btn {
+    flex-shrink: 0;
+    display: block;
+    cursor: pointer;
+    background: black;
+    color: white;
+    height: 60px;
+    width: 200px;
+    margin-bottom: 16px;
+    line-height: 60px;
+    font-size: 20px;
+    text-align: center;
+  }
+}
+</style>
